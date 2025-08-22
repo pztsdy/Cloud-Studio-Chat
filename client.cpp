35d50434f1c52aca94b9cdb0e359104c3f0af134
piaoztsdy <Piaoztsdy@outlook.com>
2025-08-22 15:12:43 +0800

    init

---
+++ client.cpp
@@ -0,0 +1,402 @@
+#define UNICODE
+#define _UNICODE
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <thread>
+#include <commctrl.h>
+#include <iostream>
+
+// version
+#define VERSION L"1.10.4"
+const std::wstring client_name = L"Cloud Studio 聊天室";
+
+// IF YOU USE MINGW, PLEASE USE THIS COMMAND
+// g++ client.cpp -o client.exe -lws2_32 -mwindows
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+
+// 用户端错误：exit(1)
+// 系统类错误：exit(2)
+// 网络错误：exit(3)
+// 其他错误：exit(4)
+
+// 定义控件ID
+#define ID_EDIT_RECV 101
+#define ID_EDIT_SEND 102
+#define ID_BUTTON_SEND 103
+#define ID_EDIT_USERNAME 104
+#define ID_EDIT_SERVERIP 105
+#define ID_BUTTON_OK 106
+#define ID_BUTTON_CANCEL 107
+#define ID_MENU_PIN 111
+#define ID_MENU_CANCEL_PIN 113
+#define ID_MENU_ABOUT 108
+#define ID_MENU_QUIT 112
+#define ID_STATIC_PORT 109
+#define ID_EDIT_PORT 110
+
+bool isPinned = false;
+HINSTANCE hInst;
+HWND hEditRecv;
+HWND hEditSend;
+SOCKET clientSocket;
+std::string serverIP;
+std::string username; // 用户名
+int serverPort = 6543;
+HFONT hFont; // 字体句柄
+
+// 创建字体
+HFONT CreateCustomFont()
+{
+    return CreateFont(
+        -14,                         // 高度
+        0,                           // 宽度
+        0,                           // 倾斜角度
+        0,                           // 旋转角度
+        FW_NORMAL,                   // 细体
+        FALSE,                       // 斜体
+        FALSE,                       // 下划线
+        FALSE,                       // 删除线
+        DEFAULT_CHARSET,             // 字符集
+        OUT_DEFAULT_PRECIS,          // 输出精度
+        CLIP_DEFAULT_PRECIS,         // 剪裁精度
+        CLEARTYPE_NATURAL_QUALITY,   // 抗锯齿
+        DEFAULT_PITCH | FF_DONTCARE, // 字符集和间距
+        L"Segoe UI Light"            // 字体名称
+    );
+}
+
+void receiveMessages()
+{
+    char buffer[32768];
+    int bytesReceived;
+    while ((bytesReceived = recv(clientSocket, buffer, 32768, 0)) > 0)
+    {
+        buffer[bytesReceived] = '\0';
+        std::string message(buffer);
+        message += "\r\n---------------------------------\r\n";
+
+        // 保存当前选择区
+        DWORD startSel = 0, endSel = 0;
+        SendMessage(hEditRecv, EM_GETSEL, (WPARAM)&startSel, (LPARAM)&endSel);
+
+        // 将光标移到末尾插入消息
+        int textLen = GetWindowTextLength(hEditRecv);
+        SendMessage(hEditRecv, EM_SETSEL, textLen, textLen);
+        SendMessageA(hEditRecv, EM_REPLACESEL, FALSE, (LPARAM)message.c_str());
+        // 恢复原选择区
+        SendMessage(hEditRecv, EM_SETSEL, startSel, endSel);
+    }
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        // 创建菜单
+        HMENU hMenu = CreateMenu();
+        HMENU hHelpMenu = CreatePopupMenu();
+        AppendMenu(hHelpMenu, MF_STRING, ID_MENU_PIN, L"置顶");
+        AppendMenu(hHelpMenu, MF_STRING, ID_MENU_CANCEL_PIN, L"取消置顶");
+        AppendMenu(hHelpMenu, MF_SEPARATOR, 0, NULL);
+        AppendMenu(hHelpMenu, MF_STRING, ID_MENU_ABOUT, L"关于");
+        AppendMenu(hHelpMenu, MF_SEPARATOR, 0, NULL);
+        AppendMenu(hHelpMenu, MF_STRING, ID_MENU_QUIT, L"退出");
+        AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hHelpMenu, L"帮助");
+        SetMenu(hwnd, hMenu);
+
+        hFont = CreateCustomFont();
+
+        hEditRecv = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+                                   WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
+                                   10, 10, 460, 300, hwnd, (HMENU)ID_EDIT_RECV, hInst, NULL);
+
+        hEditSend = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+                                   WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL,
+                                   10, 320, 360, 40, hwnd, (HMENU)ID_EDIT_SEND, hInst, NULL);
+
+        HWND hButtonSend = CreateWindow(L"BUTTON", L"发送",
+                                        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                                        380, 320, 90, 40, hwnd, (HMENU)ID_BUTTON_SEND, hInst, NULL);
+
+        // 设置控件字体
+        SendMessage(hEditRecv, WM_SETFONT, (WPARAM)hFont, TRUE);
+        SendMessage(hEditSend, WM_SETFONT, (WPARAM)hFont, TRUE);
+        SendMessage(hButtonSend, WM_SETFONT, (WPARAM)hFont, TRUE);
+        break;
+    }
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == ID_BUTTON_SEND)
+        {
+            char buffer[32768];
+            GetWindowTextA(hEditSend, buffer, 32768);
+            if (strlen(buffer) > 0)
+            {
+                std::string msgToSend = "[" + username + "]: " + buffer;
+                send(clientSocket, msgToSend.c_str(), msgToSend.length(), 0);
+                SetWindowText(hEditSend, L"");
+            }
+        }
+        else if (LOWORD(wParam) == ID_MENU_PIN)
+        {
+            isPinned = true;
+            SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+            HMENU hMenu = GetMenu(hwnd);
+            HMENU hSubMenu = GetSubMenu(hMenu, 0);
+            CheckMenuItem(hSubMenu, ID_MENU_PIN, MF_CHECKED);
+            CheckMenuItem(hSubMenu, ID_MENU_CANCEL_PIN, MF_UNCHECKED);
+        }
+        else if (LOWORD(wParam) == ID_MENU_CANCEL_PIN)
+        {
+            isPinned = false;
+            SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+            HMENU hMenu = GetMenu(hwnd);
+            HMENU hSubMenu = GetSubMenu(hMenu, 0);
+            CheckMenuItem(hSubMenu, ID_MENU_PIN, MF_UNCHECKED);
+            CheckMenuItem(hSubMenu, ID_MENU_CANCEL_PIN, MF_UNCHECKED);
+        }
+        else if (LOWORD(wParam) == ID_MENU_ABOUT)
+        {
+            std::wstring aboutMsg = client_name + L"\n作者: Piaoztsdy (Luogu UID: 1252756)\n版本: " + VERSION;
+            MessageBoxW(hwnd, aboutMsg.c_str(), L"关于", MB_OK | MB_ICONINFORMATION);
+        }
+        else if (LOWORD(wParam) == ID_MENU_QUIT)
+        {
+            PostQuitMessage(0);
+        }
+        break;
+
+    case WM_DESTROY:
+        DeleteObject(hFont); // 删除字体
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK InputDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    static HFONT hDlgFont = NULL;
+
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        hDlgFont = CreateCustomFont();
+
+        // 创建对话框控件
+        HWND hLabelUser = CreateWindow(L"STATIC", L"用户名:",
+                                       WS_CHILD | WS_VISIBLE,
+                                       10, 10, 70, 25, hwnd, NULL, hInst, NULL);
+
+        HWND hEditUser = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+                                        WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+                                        90, 10, 180, 30, hwnd, (HMENU)ID_EDIT_USERNAME, hInst, NULL);
+
+        HWND hLabelIP = CreateWindow(L"STATIC", L"服务器IP:",
+                                     WS_CHILD | WS_VISIBLE,
+                                     10, 40, 70, 25, hwnd, NULL, hInst, NULL);
+
+        HWND hEditIP = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+                                      WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+                                      90, 40, 180, 30, hwnd, (HMENU)ID_EDIT_SERVERIP, hInst, NULL);
+
+        HWND hBtnOK = CreateWindow(L"BUTTON", L"确定",
+                                   WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+                                   90, 80, 80, 25, hwnd, (HMENU)ID_BUTTON_OK, hInst, NULL);
+
+        HWND hBtnCancel = CreateWindow(L"BUTTON", L"取消",
+                                       WS_CHILD | WS_VISIBLE,
+                                       180, 80, 80, 25, hwnd, (HMENU)ID_BUTTON_CANCEL, hInst, NULL);
+
+        // 设置对话框控件字体
+        SendMessage(hLabelUser, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+        SendMessage(hEditUser, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+        SendMessage(hLabelIP, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+        SendMessage(hEditIP, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+        SendMessage(hBtnOK, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+        SendMessage(hBtnCancel, WM_SETFONT, (WPARAM)hDlgFont, TRUE);
+
+        SetFocus(hEditUser);
+        break;
+    }
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case ID_BUTTON_CANCEL:
+            PostQuitMessage(0);
+            break;
+        case ID_BUTTON_OK:
+        {
+            char userBuffer[80], ipBuffer[40]; // 不需要 portBuffer
+            GetDlgItemTextA(hwnd, ID_EDIT_USERNAME, userBuffer, 80);
+            GetDlgItemTextA(hwnd, ID_EDIT_SERVERIP, ipBuffer, 40);
+
+            if (strlen(userBuffer) == 0 || strlen(ipBuffer) == 0)
+            {
+                MessageBox(hwnd, L"请填写完整信息", L"提示", MB_OK | MB_ICONERROR);
+                exit(1);
+            }
+
+            username = userBuffer;
+            serverIP = ipBuffer;
+
+            serverPort = 6543; // 默认端口
+            DestroyWindow(hwnd);
+            break;
+        }
+        }
+        break;
+    case WM_CLOSE:
+        DeleteObject(hDlgFont); // 删除字体
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    hInst = hInstance;
+
+    // 注册输入对话框窗口类
+    WNDCLASSEX wcDlg = {0};
+    wcDlg.cbSize = sizeof(WNDCLASSEX);
+    wcDlg.lpfnWndProc = InputDlgProc;
+    wcDlg.hInstance = hInstance;
+    wcDlg.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcDlg.lpszClassName = L"InputDialog";
+    RegisterClassEx(&wcDlg);
+
+    // 创建并显示输入对话框
+    HWND hDlg = CreateWindowEx(
+        WS_EX_DLGMODALFRAME,
+        L"InputDialog",
+        L"登录",
+        WS_POPUP | WS_CAPTION | WS_SYSMENU,
+        (GetSystemMetrics(SM_CXSCREEN) - 300) / 2, // 居中显示
+        (GetSystemMetrics(SM_CYSCREEN) - 200) / 2,
+        300, 150,
+        NULL, NULL, hInstance, NULL);
+
+    if (!hDlg)
+    {
+        MessageBoxW(NULL, L"无法创建登录对话框", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        exit(2);
+    }
+
+    ShowWindow(hDlg, SW_SHOW);
+    UpdateWindow(hDlg);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+        if (!IsWindow(hDlg))
+            break;
+    }
+
+    // 只判断 serverIP
+    if (serverIP.empty())
+    {
+        MessageBoxW(NULL, L"未输入服务器IP，程序退出。", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        exit(1);
+    }
+
+    // 注册主窗口类
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = L"ChatWindow";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBoxW(NULL, L"注册窗口失败！", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        exit(2);
+    }
+    // 创建主窗口
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        L"ChatWindow",
+        client_name.c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 430,
+        NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBoxW(NULL, L"创建窗口失败！", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        exit(2);
+    }
+
+    // 初始化Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        MessageBoxW(NULL, L"WSAStartup失败！", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        exit(3);
+    }
+
+    // 创建套接字并连接到服务器
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+
+    // 检查 serverIP 是否为空
+    if (serverIP.empty())
+    {
+        MessageBoxW(NULL, L"服务器IP为空，无法连接！", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        closesocket(clientSocket);
+        WSACleanup();
+        exit(1);
+    }
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP.c_str());
+
+    if (connect(clientSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        MessageBoxW(NULL, L"连接失败！", L"错误", MB_ICONEXCLAMATION | MB_OK);
+        closesocket(clientSocket);
+        WSACleanup();
+        exit(3);
+    }
+
+    // 连接成功后立即发送用户名
+    send(clientSocket, username.c_str(), username.length(), 0);
+
+    std::thread(receiveMessages).detach();
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return (int)msg.wParam;
+}
---
+++ server.cpp
@@ -0,0 +1,212 @@
+#define UNICODE
+#define _UNICODE
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <winsock2.h>
+#include <algorithm>
+#include <map>
+
+// compile command: g++ server.cpp -o server.exe -lws2_32 -mconsole
+
+#pragma comment(lib, "ws2_32.lib")
+std::vector<SOCKET> clients;
+std::mutex clients_mutex;
+std::map<SOCKET, std::pair<std::string, int>> clientInfo; // 存储用户名和 ID
+int nextClientId = 1;                                     // 用于分配唯一 ID
+
+const std::wstring VERSION = L"1.4.0";
+const std::wstring SERVER_NAME = L"Cloud Studio 聊天室";
+
+// 获取在线人数
+int getOnlineFriends()
+{
+    std::lock_guard<std::mutex> guard(clients_mutex);
+    return clients.size();
+}
+
+void broadcastMessage(const std::string &message)
+{
+    // 在服务端控制台输出消息
+    std::cout << "[MESSAGE]: \n" << message << "\n>  " << std::flush;
+    std::lock_guard<std::mutex> guard(clients_mutex);
+    for (SOCKET client : clients)
+    {
+        send(client, message.c_str(), message.size(), 0);
+    }
+}
+
+void listOnlineFriends(SOCKET clientSocket)
+{
+    std::lock_guard<std::mutex> guard(clients_mutex);
+    std::string message = "Online Users:\n";
+    for (const auto &client : clientInfo)
+    {
+        message += "Username: " + client.second.first + ", ID: " + std::to_string(client.second.second) + ", Socket: " + std::to_string(client.first) + "\n";
+    }
+    send(clientSocket, message.c_str(), message.size(), 0);
+}
+
+void kickUser(const std::string &identifier)
+{
+    std::lock_guard<std::mutex> guard(clients_mutex);
+    for (auto it = clientInfo.begin(); it != clientInfo.end(); ++it)
+    {
+        if (it->second.first == identifier || std::to_string(it->second.second) == identifier)
+        {
+            SOCKET clientSocket = it->first;
+            send(clientSocket, "You have been kicked out by the admin.\n", 43, 0);
+            closesocket(clientSocket);
+            clients.erase(std::remove(clients.begin(), clients.end(), clientSocket), clients.end());
+            clientInfo.erase(it);
+            break;
+        }
+    }
+}
+
+void sendAdminMessage(const std::string &message)
+{
+    std::string adminMessage = "{[Server] Admin}: " + message;
+    broadcastMessage(adminMessage);
+}
+
+void handleClient(SOCKET clientSocket)
+{
+    char buffer[32768];
+    int bytesReceived;
+
+    std::string username;
+    // 首次接收用户名
+    bytesReceived = recv(clientSocket, buffer, 32768, 0);
+    if (bytesReceived <= 0)
+    {
+        closesocket(clientSocket);
+        return;
+    }
+    buffer[bytesReceived] = '\0';
+    username = buffer;
+    // 去除末尾换行和回车
+    while (!username.empty() && (username.back() == '\n' || username.back() == '\r'))
+    {
+        username.pop_back();
+    }
+
+    {
+        std::lock_guard<std::mutex> guard(clients_mutex);
+        clientInfo[clientSocket] = {username, nextClientId++};
+    }
+
+    // 用户进入时在服务端输出，并且广播
+    broadcastMessage(username + " has joined.");
+
+    while ((bytesReceived = recv(clientSocket, buffer, 32768, 0)) > 0)
+    {
+        buffer[bytesReceived] = '\0';
+        std::string message(buffer);
+
+        if (message == "/list")
+        {
+            listOnlineFriends(clientSocket);
+        }
+        else if (message.rfind("/kick ", 0) == 0)
+        {
+            std::string identifier = message.substr(6);
+            kickUser(identifier);
+        }
+        else if (message.rfind("/adminmsg ", 0) == 0)
+        {
+            std::string adminMessage = message.substr(10);
+            sendAdminMessage(adminMessage);
+        }
+        else
+        {
+            broadcastMessage(message);
+        }
+    }
+
+    closesocket(clientSocket);
+    std::lock_guard<std::mutex> guard(clients_mutex);
+    clients.erase(std::remove(clients.begin(), clients.end(), clientSocket), clients.end());
+    clientInfo.erase(clientSocket);
+}
+
+// 接收到获取人数的请求
+void handleOnlineFriends(SOCKET clientSocket)
+{
+    int onlineFriends = getOnlineFriends();
+    std::string message = std::to_string(onlineFriends);
+    send(clientSocket, message.c_str(), message.size(), 0);
+}
+
+int main()
+{
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(6543);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, 5);
+
+    std::cout << "Server started on port 6543" << std::endl;
+
+    // 服务端命令线程
+    std::thread([]()
+                {
+        std::string cmd;
+        while (true) {
+            std::cout << ">  " << std::flush;
+            std::getline(std::cin, cmd);
+            if (cmd == "list") {
+                std::lock_guard<std::mutex> guard(clients_mutex);
+                std::cout << "Online Users:" << std::endl;
+                for (const auto &client : clientInfo) {
+                    std::cout << "Username: " << client.second.first
+                              << ", ID: " << client.second.second
+                              << ", Socket: " << client.first << std::endl;
+                }
+            } else if (cmd.rfind("kick ", 0) == 0) {
+                std::string identifier = cmd.substr(5);
+                kickUser(identifier);
+                std::cout << "Kick command sent for: " << identifier << std::endl;
+            } else if (cmd.rfind("adminmsg ", 0) == 0) {
+                std::string msg = cmd.substr(9);
+                sendAdminMessage(msg);
+                std::cout << "Admin message sent." << std::endl;
+            } else if (cmd == "helpqwq") {
+                std::cout << "Commands:\n"
+                          << "list                  - Show online users\n"
+                          << "kick <name|id>        - Kick user by name or id\n"
+                          << "adminmsg <message>    - Broadcast admin message\n"
+                          << "helpqwq               - Show this help\n"
+                          << "exit                  - Shut down the server\n";
+            } else if (cmd == "exit" || cmd == "quit") {
+                broadcastMessage("服务器已经关闭了哦~");
+                std::cout << "Shutting down server..." << std::endl;
+                WSACleanup();
+                exit(0);
+            } else {
+                std::cout << "Unknown command: " << cmd << std::endl;
+            }
+        } })
+        .detach();
+
+    while (true)
+    {
+        SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+        std::lock_guard<std::mutex> guard(clients_mutex);
+        clients.push_back(clientSocket);
+
+        std::thread(handleClient, clientSocket).detach();
+    }
+
+    WSACleanup();
+    return 0;
+}
